a5700606ce8d2fc1ca2299167e2ce3eb0decf101
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-08 16:41:43 +0900

    feat: しおりの処理に場所追加の処理も追加

--- www/api/create-bookmark/index.php
+++ www/api/create-bookmark/index.php
@@ -13,18 +13,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     exit;
 }
 
-$bookmark_name = $_POST["bookmark-name"];
-$bookmark_description = $_POST["bookmark-description"];
-$user_id = $_SESSION["user_id"];
-
-if (!isset($bookmark_name) || empty($bookmark_name)) {
+if (!isset($_POST["bookmark-name"]) || empty($_POST["bookmark-name"])) {
     header("Location: /create-bookmark?error=1");
     die("Error: bookmark_name is null or empty");
 }
-if (!isset($bookmark_description) || empty($bookmark_description)) {
+if (!isset($_POST["bookmark-description"]) || empty($_POST["bookmark-description"])) {
     header("Location: /create-bookmark?error=2");
     die("Error: bookmark_description is null or empty");
 }
+
+if (!isset($_POST["bookmark_content"]) || empty($_POST["bookmark_content"]) || !is_array($_POST["bookmark_content"])) {
+    header("Location: /create-bookmark?error=3");
+    die("Error: bookmark_content is null or empty");
+}
+
+$bookmark_name = $_POST["bookmark-name"];
+$bookmark_description = $_POST["bookmark-description"];
+$bookmark_content = $_POST["bookmark_content"];
+$user_id = $_SESSION["user_id"];
 
 try {
     $pdo = connect_db();
@@ -36,9 +42,25 @@ try {
 
     $bookmark_id = $pdo->lastInsertId();
 
+    // bookmark_content追加
+    foreach ($bookmark_content as $key => $value) {
+        // 配列内の各要素を処理する
+        if (is_array($value)) {
+            // ネストされた配列の場合
+            $stmt = $pdo->prepare("INSERT INTO bookmark_content ( bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url ) values (:bookmark_content_name, :bookmark_content_address, :bookmark_content_comment, :bookmark_content_price, :bookmark_id, '-')");
+            $stmt->bindParam(':bookmark_content_name', $value["place-name"], PDO::PARAM_STR);
+            $stmt->bindParam(':bookmark_content_address', $value["place-address"], PDO::PARAM_STR);
+            $stmt->bindParam(':bookmark_content_comment', $value["place-comment"], PDO::PARAM_STR);
+            $stmt->bindParam(':bookmark_content_price', $value["expenses"], PDO::PARAM_INT);
+            $stmt->bindParam(':bookmark_id', $bookmark_id, PDO::PARAM_INT);
+            // $stmt->bindParam(':bookmark_instagram_url', "-", PDO::PARAM_STR);
+            $stmt->execute();
+        }
+    }
+
     header("Location: /create-bookmark?success");
 
 } catch (PDOException $e) {
-    // header("Location: /create-bookmark?error=5");
+    header("Location: /create-bookmark?error=5");
     echo $e->getMessage();
 }
---
+++ www/js/create-bookmark.js
@@ -0,0 +1,69 @@
+window.addEventListener('load', () => {
+    let placeCount = 0;
+    document.querySelector('#placeAddBtn').addEventListener('click', () => {
+        // 値取得
+        const place_name = document.getElementById('place-name').value
+        const place_address = document.getElementById('place-address').value
+        const place_comment = document.getElementById('place-comment').value
+        const expenses = document.getElementById('expenses').value
+
+        if (
+            !place_name ||
+            !place_address ||
+            !place_comment ||
+            !expenses
+        ) {
+            alert('未入力の項目があります。')
+            return
+        }
+
+        console.log({
+            place_name,
+            place_address,
+            place_comment,
+            expenses
+        });
+
+        // HTMLにhiddenで追加
+        const place_name_input = document.createElement('input')
+        place_name_input.name = `bookmark_content[${placeCount}][${document.getElementById('place-name').id}]`
+        place_name_input.type="hidden"
+        place_name_input.value = place_name
+        const place_address_input = document.createElement('input')
+        place_address_input.name = `bookmark_content[${placeCount}][${document.getElementById('place-address').id}]`
+        place_address_input.type="hidden"
+        place_address_input.value = place_address
+        const place_comment_input = document.createElement('input')
+        place_comment_input.name = `bookmark_content[${placeCount}][${document.getElementById('place-comment').id}]`
+        place_comment_input.type="hidden"
+        place_comment_input.value = place_comment
+        const expenses_input = document.createElement('input')
+        expenses_input.name = `bookmark_content[${placeCount}][${document.getElementById('expenses').id}]`
+        expenses_input.type="hidden"
+        expenses_input.value = expenses
+
+        document.getElementById('place').appendChild(place_name_input)
+        document.getElementById('place').appendChild(place_address_input)
+        document.getElementById('place').appendChild(place_comment_input)
+        document.getElementById('place').appendChild(expenses_input)
+
+        document.getElementById('place-name').value= ""
+        document.getElementById('place-address').value= ""
+        document.getElementById('place-comment').value= ""
+        document.getElementById('expenses').value= ""
+        document.querySelector('#addPlaceModal .btn-close').click()
+
+        // // アコーディオン追加
+        // const accordion = document.getElementById('accordionPanelsStayOpenExample')
+        // // スライダーのテンプレートのタグを取得
+        // const template = document.getElementById("accordion-item-template");
+        // // 複製
+        // const clone = template.content.cloneNode(true);
+        // // previewにスライダーを挿入
+        // accordion.appendChild(clone);
+        // // スライダーの中のスライドを入れる部分を取得
+        // const accordion_inner = document.querySelector("#preview .carousel-inner");
+
+        placeCount++;
+    })
+})

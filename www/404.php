961526f4c7820cca550a98e6c5e15c2fc9b31ec5
きのこ <kinokonokaibutu@gmail.com>
2024-03-09 13:44:16 +0900

    404スタイル作成

--- www/404.php
+++ www/404.php
@@ -7,14 +7,29 @@
     <title>404 Not Found</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/404.css">
     <script src="/js/main.js"></script>
 </head>
 
 <body>
     <main class="container row m-auto">
-        <h1 class="text-center py-3">ページが存在しません。</h1>
-        <a href="/" class="link-primary text-center">TOPへ戻る</a>
+        <div class="message">
+            <h1 class="text-center py-3">ページが存在しません。</h1>
+            <a href="/" class="link-primary text-center">サインイン画面に戻る</a>
+        </div>
+
+        <img src="/img/backgrounding.png" class="bg-img" alt="" srcset="">
+
+        <!-- <h1 class="header">考えられる原因<h1>
+            
+        <font size="5" class="pos" >◯リンクが更新されている可能性がある。</font>
+        <br>
+        <font size="5" class="pos">◯存在しない。</font>
+        <br>
+        <font size="5" class="pos">◯など。</font>
+        <img src="/img/404.jpeg"  alt="404" class="pic"> -->
+
+        
     </main>
 </body>
 

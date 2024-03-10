5b5fd1a51d2fc87e92d283f23ab62ff5f84fa2dc
きのこ <kinokonokaibutu@gmail.com>
2024-03-10 17:17:24 +0900

    ルイスで京都観光追加　写真3枚

--- initdb.d/create-table.sql
+++ initdb.d/create-table.sql
@@ -60,6 +60,18 @@ VALUES ('JohnDoe2', SHA2('password', 256), 'john-doe2@example.com');
 INSERT INTO user (user_name, password, email)
 VALUES ('wataru', SHA2('aaaa', 256), 'wataru@example.com');
 
+INSERT INTO user (user_name, password, email)
+VALUES ('LionelMessi', SHA2('11', 256), 'goot@example.com');
+
+INSERT INTO user (user_name, password, email)
+VALUES ('gotumoritakesi', SHA2('gotumori', 256), 'takesi@example.com');
+
+INSERT INTO user (user_name, password, email)
+VALUES ('hetaredaimaou', SHA2('Yu1128145', 256), 'hanadekayu@icloud.com');
+
+INSERT INTO user (user_name, password, email)
+VALUES ('Luis Suárez ', SHA2('soccer', 256), 'soccerolayer@icloud.com');
+
 -- しおりを追加
 -- INSERT INTO bookmark (bookmark_name, bookmark_description, user_id)
 -- VALUES ('Test Bookmark', 'This is a test bookmark', 1);
@@ -84,6 +96,15 @@ VALUES ('博多旅行', '美味しい料理', 2);
 
 INSERT INTO bookmark (bookmark_name, bookmark_description, user_id)
 VALUES ('沖縄', '沖縄美ら海水族館', 2);
+
+INSERT INTO bookmark (bookmark_name, bookmark_description, user_id)
+VALUES ('日帰り愛知', '名所巡り', 6);
+
+INSERT INTO bookmark (bookmark_name, bookmark_description, user_id)
+VALUES ('みんなでドライブ', '免許持ってないけど！', 5);
+
+INSERT INTO bookmark (bookmark_name, bookmark_description, user_id)
+VALUES ('京都花見', '#京都 ＃観光', 7);
 
 
 -- しおり内のコンテンツを追加
@@ -157,6 +178,76 @@ how_move
 )
 VALUES ('沖縄旅行', '〒905-0206 沖縄県国頭郡本部町石川４２４', '美ら海水族館', 19700, 5, 'https://www.instagram.com/test_content', '/img/sample1.jpg', 1, 20, 3000, 0, 20, "地下鉄で移動");
 
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('名古屋駅', '〒450-0002愛知県名古屋市中村区名駅1丁目1-4', '駅めっちゃきれい', 2500, 6, 'https://www.instagram.com/test_content', '/img/sample1.jpg', 0, 30, 1000, 2, 30, "新快速で移動");
+
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('竹島', '〒450-0002愛知県名古屋市中村区名駅1丁目1-4', '夕日めちゃきれい', 1000, 6, 'https://www.instagram.com/test_content', '/img/sample1.jpg', 1, 30, 50, 1, 00, "名古屋駅から電車");
+
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('淡路島までドライブ', '淡路島', 'ドライブたのしい', 2000, 7, 'https://www.instagram.com/test_content', '/img/sample1.jpg', 0, 20, 0, 2, 00, "車");
+
+
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('京都花見', '城南宮', '花綺麗', 1100, 7, 'https://www.instagram.com/test_content', '/img/Jonangu.jpg', 1, 30, 1100, 0, 10, "徒歩");
+
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('京都花見', '庭園', '綺麗', 0, 7, 'https://www.instagram.com/test_content', '/img/Kyoyto Hanami Garden.jpg', 0, 30, 0, 0, 10, "徒歩");
+
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('京都花見', '下鴨神社', '綺麗', 0, 7, 'https://www.instagram.com/test_content', '/img/Kyoto Han.jpg', 1, 00, 0, 0, 40, "電車");
+
+INSERT INTO bookmark_content (bookmark_content_name, bookmark_content_address, bookmark_content_comment, bookmark_content_price, bookmark_id, bookmark_instagram_url, bookmark_content_image_path,
+stay_time_h,
+stay_time_m,
+used_money,
+move_time_h,
+move_time_m,
+how_move
+)
+VALUES ('京都花見', '北野天満宮', '花が綺麗', 0, 7, 'https://www.instagram.com/test_content', '/img/Kyoto.jpg', 0, 30, 0, 0, 10, "徒歩");
 -- しおり内コンテンツの画像を追加
 -- INSERT INTO bookmark_content_image (bookmark_content_image_path, bookmark_content_id)
 -- VALUES ('/image/sample1.jpg', 1);

40e9e77d4523894cf736e4a61adfc57344f56803
hetaredaimaou <hanadekayu@icloud.com>
2024-03-10 10:39:24 +0900

    a

--- www/bookmark/index.php
+++ www/bookmark/index.php
@@ -50,19 +50,26 @@ if (isset($_GET["id"]) && $_GET["id"] !== "") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>しおり</title>
     <!-- BootStrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/bookmark.css" />
+    <link rel="stylesheet" href="/css/okamotobookmark.css" />
     <script src="/js/main.js"></script>
     <script src="/js/okamoto.js"></script>
 </head>
+
 <body>
     <main class="container row m-auto">
-        <h1 class="text-center text-white"><?= $currentBookmark["bookmark_name"] ?></h1>
-        <?php 
-            $cards = new Cards($currentBookmark["bookmark_id"]);
-            $cards->render();
+        <h1 class="text-center text-white">
+            <?= $currentBookmark["bookmark_name"] ?>
+        </h1>
+        <?php
+        $cards = new Cards($currentBookmark["bookmark_id"]);
+        $cards->render();
         ?>
     </main>
 </body>
